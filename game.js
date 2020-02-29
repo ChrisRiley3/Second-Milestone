1ab0b2d48a36649b3817be1b0e0c38be9acc24be
Christopher Riley <chris_riley1993@hotmail.com>
2020-02-29 11:17:41 +0000

    created moving image effect on background

--- game.js
+++ game.js
@@ -12,22 +12,28 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload() {
+    //Loads images to the canvas 
     this.load.image('background', 'assets/images/background.png');
-    this.load.image('desert', 'assets/images/Desert.png');
-    this.load.image('forest', 'assets/images/Forest.png');
-    this.load.image('lava', 'assets/images/Lava.png');
-    this.load.image('ocean', 'assets/images/Ocean.png');
-    this.load.image('terran', 'assets/images/Terran.png');
-}
+    this.load.image('planet', 'assets/images/Desert.png');
+    this.load.image('planet1', 'assets/images/Forest.png');
+    this.load.image('planet2', 'assets/images/Lava.png');
+    this.load.image('planet3', 'assets/images/Ocean.png');
+    this.load.image('planet4', 'assets/images/Terran.png');
+};
 
 function create() {
-    this.add.image(400, 300, 'background').setScale(2);
-    this.add.image(config.width / 2, config.height / 6, 'desert').setScale(0.9);
-    this.add.image(config.width / 2, config.height / 6 + 100, 'forest').setScale(1.2);
-    this.add.image(config.width / 2, config.height / 2, 'lava').setScale(0.7);
-    this.add.image(config.width / 2, config.height / 2 + 100, 'ocean').setScale(0.5);
-    this.add.image(config.width / 2, config.height - 100, 'terran').setScale(1.5);
-}
+    //Add's images to the canvas
+    this.background = this.add.tileSprite(400, 300, config.width, config.height, 'background').setScale(2);
+    this.planet = this.add.image(config.width / 2, config.height / 6, 'planet').setScale(0.9);
+    this.planet1 = this.add.image(config.width / 2, config.height / 6 + 100, 'planet1').setScale(1.2);
+    this.planet2 = this.add.image(config.width / 2, config.height / 2, 'planet2').setScale(0.7);
+    this.planet3 = this.add.image(config.width / 2, config.height / 2 + 100, 'planet3').setScale(0.5);
+    this.planet4 = this.add.image(config.width / 2, config.height - 100, 'planet4').setScale(1.5);
+};
+
 function update() {
-    
-}
+    //Creates a moving image effect on the background
+    this.background.tilePositionX -= 0.5;
+};
+
+
