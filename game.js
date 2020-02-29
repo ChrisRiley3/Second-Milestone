6ac4139d78ca45c853afc25c8f7329e8bd104f87
Christopher Riley <chris_riley1993@hotmail.com>
2020-02-29 08:54:20 +0000

    added planets to game canvas and deleted unwanted image from assets

--- game.js
+++ game.js
@@ -13,18 +13,21 @@ var game = new Phaser.Game(config);
 
 function preload() {
     this.load.image('background', 'assets/images/background.png');
-    this.load.image('baren', 'assets/images/baren.png');
-    this.load.image('desert', 'assets/images/desert.png');
-    this.load.image('forest', 'assets/images/forest.png');
-    this.load.image('lava', 'assers/images/lava.png');
-    this.load.image('ocean', 'assets/images/ocean.png');
-    this.load.image('terran', 'assets/images/terrran.png');
+    this.load.image('desert', 'assets/images/Desert.png');
+    this.load.image('forest', 'assets/images/Forest.png');
+    this.load.image('lava', 'assets/images/Lava.png');
+    this.load.image('ocean', 'assets/images/Ocean.png');
+    this.load.image('terran', 'assets/images/Terran.png');
 }
 
 function create() {
     this.add.image(400, 300, 'background').setScale(2);
+    this.add.image(config.width / 2, config.height / 6, 'desert');
+    this.add.image(config.width / 2, config.height / 6 + 100, 'forest');
+    this.add.image(config.width / 2, config.height / 2, 'lava');
+    this.add.image(config.width / 2, config.height / 2 + 100, 'ocean');
+    this.add.image(config.width / 2, config.height - 100, 'terran');
 }
-
 function update() {
     
 }
