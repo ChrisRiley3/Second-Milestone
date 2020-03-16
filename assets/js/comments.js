6029dce927416c9d0ab096cf370cb92beefd1ec4
Christopher Riley <chris_riley1993@hotmail.com>
2020-03-16 16:49:10 +0000

    added javascript to my comments form so comment store in local storage

---
+++ assets/js/comments.js
@@ -0,0 +1,34 @@
+// https://www.youtube.com/watch?v=ws6yQ4yKNG0&t=515s this code came from this source 
+function render(data) {
+  var html ='<div class="comment-box"><div class="comment-panel"><span>'+data.name+'</span><div class="date">'+data.date +'</div><p>'+data.body+'</p></div></div>';
+  $('#myContainer').append(html);
+}
+
+$(document).ready(function() {
+
+    var comment = []
+
+    if(!localStorage.commentData) {
+        localStorage.commentData = [];
+    } else {
+        comment = JSON.parse(localStorage.commentData);
+    }
+
+    for (var i = 0; i < comment.length; i++) {
+        render(comment[i]);
+  }
+
+  $('#addComment').click(function(){
+    var addObj = {
+        'name': $('#name').val(),
+        'date': $('#date').val(),
+        'body': $('#bodyText').val()
+    }
+    comment.push(addObj);
+    localStorage.commentData = JSON.stringify(comment);
+    render(addObj);
+    $('#name').val('');
+    $('#date').val('dd/mm/yyyy');
+    $('#bodyText').val('')
+  });
+});
